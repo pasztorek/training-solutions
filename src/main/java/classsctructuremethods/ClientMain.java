a4172a5972da2b522cd97fe18a325c8702700f38
pgk <pgk977@outlook.com>
2020-10-29 20:50:25 +0100

    2.

---
+++ src/main/java/classsctructuremethods/ClientMain.java
@@ -0,0 +1,20 @@
+package classsctructuremethods;
+
+public class ClientMain {
+
+    public static void main(String[] args) {
+
+        Client kliens = new Client();
+        kliens.setName("Gabi");
+        kliens.setYear(20);
+        kliens.setAddress("London");
+
+        System.out.println(kliens.getName() + kliens.getYear() + kliens.getAddress());
+
+        kliens.migrate("Barcelona");
+        System.out.println(kliens.getName() + kliens.getYear() + kliens.getAddress());
+
+
+    }
+
+}
