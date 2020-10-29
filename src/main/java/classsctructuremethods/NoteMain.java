7e52e3a92940fd21a53c32af48d09900546c4b73
pgk <pgk977@outlook.com>
2020-10-29 21:05:17 +0100

    2.

---
+++ src/main/java/classsctructuremethods/NoteMain.java
@@ -0,0 +1,14 @@
+package classsctructuremethods;
+
+public class NoteMain {
+    public static void main(String[] args) {
+
+        Note jegyzet = new Note();
+        jegyzet.setName("Gábor ");
+        jegyzet.setTopic("Java ");
+        jegyzet.setText("nagyon nehéz ");
+
+        System.out.println(jegyzet.getNoteText());
+    }
+
+}
