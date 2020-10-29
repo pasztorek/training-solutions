7141b4095360b6fb728f449d3a5201247d4c9ef2
pgk <pgk977@outlook.com>
2020-10-29 14:11:48 +0100

    2.

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -1,7 +1,17 @@
 package classstructureio;
+import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+
+        Scanner scannerA = new Scanner(System.in);
+        System.out.println("Kérek egy számot");
+        int a = scannerA.nextInt();
+
+        Scanner scannerB = new Scanner(System.in);
+        System.out.println("Kérek egy másik számot");
+        int b = scannerB.nextInt();
+
 
     }
 }
