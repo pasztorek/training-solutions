e7f056cb75973da856c8474c212d3deae9556695
pgk <pgk977@outlook.com>
2020-10-30 20:32:59 +0100

    2.

---
+++ src/main/java/classsctructureintegrate/BankAccount.java
@@ -0,0 +1,34 @@
+package classsctructureintegrate;
+
+public class BankAccount {
+
+    private String owner;
+    private String accountNumber;
+    private int balance;
+
+    public BankAccount(String owner, String accountNumber, int balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit (int amount){
+        this.balance = balance + amount;
+
+    }
+
+    public void withdraw (int amount){
+        this.balance = balance - amount;
+
+    }
+
+    public void transfer (BankAccount to, int amount) {
+        this.balance = balance - amount;
+
+    }
+    public String getInfo (){
+
+        return owner + " (" + accountNumber + ") : " + balance + " Ft";
+
+    }
+}
---
+++ src/main/java/classsctructureintegrate/BankAccountMain.java
@@ -0,0 +1,45 @@
+package classsctructureintegrate;
+
+import java.util.Scanner;
+
+public class BankAccountMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Első bankszámla tulajdonosa:");
+        Scanner account1Owner = new Scanner(System.in);
+        String Owner1 = account1Owner.nextLine();
+
+
+        System.out.println("Első bankszámla szama:");
+        Scanner account1Number = new Scanner(System.in);
+        String Number1 = account1Number.nextLine();
+
+        System.out.println("Első bankszámla egyenlege:");
+        Scanner account1balance = new Scanner(System.in);
+        int Balance1 = account1balance.nextInt();
+
+        BankAccount szamla1 = new BankAccount(Owner1, Number1, Balance1);
+
+        System.out.println("Második bankszámla tulajdonosa:");
+        Scanner account2Owner = new Scanner(System.in);
+        String Owner2 = account2Owner.nextLine();
+
+        System.out.println("Második bankszámla szama:");
+        Scanner account2Number = new Scanner(System.in);
+        String Number2 = account2Number.nextLine();
+
+        System.out.println("Második bankszámla egyenlege:");
+        Scanner account2balance = new Scanner(System.in);
+        int Balance2 = account2balance.nextInt();
+
+        BankAccount szamla2 = new BankAccount(Owner2, Number2, Balance2);
+
+
+
+
+
+        System.out.println(szamla1.getInfo());
+        System.out.println(szamla2.getInfo());
+    }
+}
