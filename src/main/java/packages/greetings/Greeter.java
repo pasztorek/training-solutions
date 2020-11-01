3fcce93d25baaaee76da0463a20980a8ac7aebaa
pgk <pgk977@outlook.com>
2020-11-01 19:51:05 +0100

    2.

--- src/main/java/packages/greetings/Greeter.java
+++ src/main/java/packages/greetings/Greeter.java
@@ -2,4 +2,13 @@ package packages.greetings;
 
 public class Greeter {
 
+    public Greeter() {
+    }
+
+    public void sayHello(){
+        System.out.println("Hello World!");
+
+
+    }
+
 }
--- src/main/java/packages/main/MainProgram.java
+++ src/main/java/packages/main/MainProgram.java
@@ -1,5 +1,14 @@
 package packages.main;
 
+import packages.greetings.Greeter;
+
 public class MainProgram {
+
+    public static void main(String[] args) {
+
+        Greeter udv = new Greeter();
+        udv.sayHello();
+
+    }
 
 }
