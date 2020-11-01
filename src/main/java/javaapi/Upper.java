4224572fcd0987cb83f85b026d43d0a5bbee11e8
pgk <pgk977@outlook.com>
2020-11-01 20:02:20 +0100

    2.

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,14 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+
+        String szoveg = "Hello World!";
+        szoveg = szoveg.toUpperCase();
+
+        System.out.println(szoveg);
+
+
+    }
+}
