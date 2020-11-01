8826d7cab93eabda337c04eaaec1a3ce0c39576e
pgk <pgk977@outlook.com>
2020-11-01 11:02:48 +0100

    2.

---
+++ src/main/java/localvariables/LocalVariablesMain.java
@@ -0,0 +1,26 @@
+package localvariables;
+
+public class LocalVariablesMain {
+
+    public static void main(String[] args) {
+
+        boolean b;
+        b = false;
+
+        int a = 2;
+        int i = 3, j = 4;
+        String s = "Hello World";
+        String t = s;
+
+
+
+        {
+            int x =0;
+            System.out.println(a);
+
+        }
+      //  System.out.println(x);
+        }
+
+    }
+
