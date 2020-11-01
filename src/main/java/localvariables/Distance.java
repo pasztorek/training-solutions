593eaf1420ed6bcc2fa208f1d3083309354a779b
pgk <pgk977@outlook.com>
2020-11-01 11:17:34 +0100

    2.

---
+++ src/main/java/localvariables/Distance.java
@@ -0,0 +1,14 @@
+package localvariables;
+
+public class Distance {
+    double distanceInKm = 3.4;
+    boolean ecaxt = true;
+
+    public double getDistanceInKm() {
+        return distanceInKm;
+    }
+
+    public boolean isEcaxt() {
+        return ecaxt;
+    }
+}
---
+++ src/main/java/localvariables/DistanceMain.java
@@ -0,0 +1,18 @@
+package localvariables;
+
+public class DistanceMain {
+
+    public static void main(String[] args) {
+
+        Distance tav = new Distance();
+
+        int x = (int) tav.distanceInKm;
+
+        System.out.println(tav.getDistanceInKm());
+        System.out.println(tav.isEcaxt());
+        System.out.println(x);
+
+
+
+    }
+}
