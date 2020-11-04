85740d031c3beeff6499099fe4fcd580c09eb42e
pgk <pgk977@outlook.com>
2020-11-04 19:15:41 +0100

    2.

--- src/main/java/arrays/ArraysMain.java
+++ src/main/java/arrays/ArraysMain.java
@@ -1,19 +1,28 @@
 package arrays;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ArraysMain {
 
     public static void main(String[] args) {
-        ArraysMain tomok = new ArraysMain();
-
+        ArraysMain tombok = new ArraysMain();
+        System.out.println(tombok.numberOfDaysAsString());
+        System.out.println(tombok.daysOfWeek());
 
     }
 
    public String numberOfDaysAsString(){
         int[] napok= {31,29,31,30,31,30,31,31,30,31,30,31};
-
         return Arrays.toString(napok);
+    }
+
+    public List<String> daysOfWeek(){
+            List<String> napok =  Arrays.asList("Hétfő", "Kedd","Szerda","Csütörtök","Péntek","Szombat","Vasárnap");
+
+
+            return napok;
     }
 }
