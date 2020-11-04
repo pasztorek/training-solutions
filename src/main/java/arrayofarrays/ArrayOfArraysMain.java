5bd094dd1378a4dce3f3aeea9b988a55036b6138
pgk <pgk977@outlook.com>
2020-11-04 17:22:11 +0100

    2.

--- src/main/java/arrayofarrays/ArrayOfArraysMain.java
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -6,7 +6,6 @@ public class ArrayOfArraysMain {
         ArrayOfArraysMain szorzotabla = new ArrayOfArraysMain();
         szorzotabla.multiplicationTable(5);
 
-
     }
 
     public int[][] multiplicationTable(int size) {
@@ -15,19 +14,15 @@ public class ArrayOfArraysMain {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 szorzo[i][j] = i * j;
-
             }
-
         }
         return szorzo;
     }
-
     public int[][] getValues() {
         int[] napok= {31,29,31,30,31,30,31,31,30,31,30,31};
         int [][] naptar = new int[12][];
         for (int i=0; i<=napok.length; i++){
             naptar[i][napok[i]] = i;
-
         }
             return naptar;
     }
---
+++ src/main/java/arrays/ArraysMain.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArraysMain {
+
+    public static void main(String[] args) {
+        ArraysMain tomok = new ArraysMain();
+
+
+    }
+
+   public String numberOfDaysAsString(){
+        int[] napok= {31,29,31,30,31,30,31,31,30,31,30,31};
+
+        return Arrays.toString(napok);
+    }
+}
