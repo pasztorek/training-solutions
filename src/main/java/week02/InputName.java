8a8ffb4ae15623ba36f261c79ffd0839ea1a38fd
pgk <pgk977@outlook.com>
2020-11-05 14:29:45 +0100

    2.

---
+++ src/main/java/week02/InputName.java
@@ -0,0 +1,25 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputName {
+
+
+
+    public static void main(String[] args) {
+
+        String nevek[] = new String[5];
+        InputName nevsor = new InputName();
+
+        for (int i=0; i<5; i++) {
+            System.out.println("nevet kérek:");
+            Scanner nevbe = new Scanner(System.in);
+            nevek[i] = nevbe.nextLine();
+        }
+        for (int i=0; i<5; i++) {
+            System.out.println(i + ". elem " + nevek[i]);
+        }
+
+
+    }
+}
