721d4aca9415c4d0059802d5aa6c74bed6a88c84
pgk <pgk977@outlook.com>
2020-11-06 15:57:17 +0100

    2.

--- src/main/java/attributes/person/Person.java
+++ src/main/java/attributes/person/Person.java
@@ -1,4 +1,30 @@
 package attributes.person;
 
 public class Person {
+
+    private String name;
+    private String identificationCard;
+    private Address address;
+
+
+
+    public Person(String name, String identificationCard) {
+        this.name = name;
+        this.identificationCard = identificationCard;
+    }
+
+    public String personToString(){
+        return name + identificationCard;
+    }
+
+    public void  correctData(String name, String identificationCard){
+        this.name = name;
+        this.identificationCard = identificationCard;
+
+    }
+    public void moveTo(Address address){
+        this.address = address;
+
+    }
+
 }
