bc12022cf71a5b869f503a6c25957ee1352d738d
pgk <pgk977@outlook.com>
2020-11-09 20:29:49 +0100

    2.

--- src/main/java/typeconversion/dataloss/DataLoss.java
+++ src/main/java/typeconversion/dataloss/DataLoss.java
@@ -4,11 +4,11 @@ public class DataLoss {
 
     public static void main(String[] args) {
         long szaml = 1;
-        float szam3 = (long) (float) szaml;
+        float szam3 = 1;
         System.out.println(Long.MAX_VALUE);
 
         for (szaml = 1; szaml < Long.MAX_VALUE; szaml++) {
-            szaml = (long) szam3;
+            szam3 = (float) szaml;
 
             if (szaml != szam3) {
 
