3eaac50bcd664f38a6b07e01632a2c80f3d0c6ab
pgk <pgk977@outlook.com>
2020-11-14 12:36:20 +0100

    2.

---
+++ src/main/java/stringbuilder/NameBuilder.java
@@ -0,0 +1,29 @@
+package stringbuilder;
+
+public class NameBuilder {
+
+
+    public String concatNameWesternStyle(String familyName, String middleName, String givenName, Title title){
+
+
+        return familyName;
+    }
+    public String concatNameHungarianStyle(String familyName, String middleName, String givenName, Title title){
+
+
+        return familyName;
+    }
+    public String insertTitle(String name, Title title, String where){
+
+
+        return name;
+    }
+    public String deleteNamePart(String name, String delete){
+
+
+        return name;
+    }
+
+
+
+}
---
+++ src/main/java/stringbuilder/Title.java
@@ -0,0 +1,15 @@
+package stringbuilder;
+
+public enum Title {
+
+    MR("Mr."), MS("Ms."), DR("Dr."), PROF("Prof");
+
+
+    public String title;
+
+    Title(String title) {
+        this.title = title;
+    }
+}
+
+
