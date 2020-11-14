1ba468e5aa1008f92e2296b5485d9dee5a768e4e
pgk <pgk977@outlook.com>
2020-11-14 16:33:37 +0100

    2.

--- src/main/java/stringbuilder/NameBuilder.java
+++ src/main/java/stringbuilder/NameBuilder.java
@@ -3,15 +3,68 @@ package stringbuilder;
 public class NameBuilder {
 
 
+    private String familyName;
+    private String middleName;
+    private String givenName;
+    private String title;
+
+
     public String concatNameWesternStyle(String familyName, String middleName, String givenName, Title title){
 
+        StringBuilder eredmeny = new StringBuilder();
 
-        return familyName;
+        if(title != null){
+            eredmeny.append(title.title);
+            eredmeny.append(" ");
+        }
+
+        if(isEmpty(familyName) || isEmpty(givenName)){
+            throw new IllegalArgumentException("Család és keresztnév kötelező");
+        }
+
+        eredmeny.append(givenName);
+        eredmeny.append(" ");
+
+
+        if(!isEmpty(middleName)) {
+            eredmeny.append(middleName);
+            eredmeny.append(" ");
+        }
+
+        eredmeny.append(familyName);
+
+
+        return eredmeny.toString();
+
+
     }
     public String concatNameHungarianStyle(String familyName, String middleName, String givenName, Title title){
 
+        StringBuilder eredmeny = new StringBuilder();
 
-        return familyName;
+        if(title != null){
+            eredmeny.append(title.title);
+            eredmeny.append(" ");
+        }
+
+        if(isEmpty(familyName) || isEmpty(givenName)){
+            throw new IllegalArgumentException("Család és keresztnév kötelező");
+        }
+
+
+        eredmeny.append(familyName);
+        eredmeny.append(" ");
+
+        if(!isEmpty(middleName)) {
+            eredmeny.append(middleName);
+            eredmeny.append(" ");
+        }
+
+
+        eredmeny.append(givenName);
+
+        return eredmeny.toString();
+
     }
     public String insertTitle(String name, Title title, String where){
 
@@ -24,6 +77,8 @@ public class NameBuilder {
         return name;
     }
 
-
+    public boolean isEmpty(String str) {
+        return str == null || "".equals(str.trim());
+    }
 
 }
