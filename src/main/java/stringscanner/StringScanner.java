0b344277825f5571ae24e11c7e174e24ed641b8a
pgk <pgk977@outlook.com>
2020-11-14 17:11:28 +0100

    2.

---
+++ src/main/java/stringscanner/StringScanner.java
@@ -0,0 +1,23 @@
+package stringscanner;
+
+public class StringScanner {
+
+    public int readAndSumValues(String intString, String delimiter){
+
+
+        return 3;
+    }
+
+    public int readAndSumValues(String intString){
+
+
+
+        return 3;
+    }
+
+    public String filterLinesWithWordOccurrences(String text, String word){
+
+
+        return text;
+    }
+}
