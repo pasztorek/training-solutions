8f857c749a385f1f4a921d24e07f4ae8a1cf1a94
pgk <pgk977@outlook.com>
2020-11-15 10:53:47 +0100

    2.

--- src/main/java/filescanner/library/Library.java
+++ src/main/java/filescanner/library/Library.java
@@ -1,4 +1,28 @@
 package filescanner.library;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Library {
+
+    List<Book> books = new ArrayList<>();
+
+    public void loadFromFile() {
+        try (Scanner scanner = new Scanner(Library.class.getResourceAsStream("/books.csv"))) {
+            scanner.useDelimiter(";|(\r\n)|\n");
+            while (scanner.hasNextLine()) {
+                  String regNum = scanner.next();
+                  String author = scanner.next();
+                  String title = scanner.next();
+                  int yearOfPublish = scanner.nextInt();
+                  books.add(new Book(regNum,author,title,yearOfPublish));
+            }
+
+        }
+    }
+    public List<Book> getBooks() {
+        return books;
+    }
 }
