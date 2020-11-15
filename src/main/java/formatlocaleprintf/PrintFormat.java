1cf70f1f5c2d8853c5cfadc40ce9c4417a2205be
pgk <pgk977@outlook.com>
2020-11-15 11:16:50 +0100

    2.

---
+++ src/main/java/formatlocaleprintf/PrintFormat.java
@@ -0,0 +1,34 @@
+package formatlocaleprintf;
+
+public class PrintFormat {
+
+    public String formatMessageWithIntegerParameters(String formatString, Integer i, Integer j){
+
+        return formatString;
+    }
+
+    public String checkException(String formatString, Integer i, Integer j){
+
+        return formatString;
+    }
+
+    public String printFormattedText(Double number){
+
+        return "";
+    }
+
+    public String printFormattedText(int count, String fruit){
+
+        return fruit;
+    }
+
+    public String printFormattedText(int number){
+
+        return "";
+    }
+
+    public String printFormattedText(Integer i, Integer j, Integer k){
+
+        return "";
+    }
+}
