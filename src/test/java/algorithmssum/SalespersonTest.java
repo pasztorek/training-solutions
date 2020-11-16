b443291d7ae6d18510c21d651f2bbaeba38dd919
pgk <pgk977@outlook.com>
2020-11-16 12:12:17 +0100

    2.

---
+++ src/test/java/algorithmssum/SalespersonTest.java
@@ -0,0 +1,17 @@
+package algorithmssum;
+
+import algorithmssum.Salesperson;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SalespersonTest {
+
+    @Test
+    public void testCreateSalesperson() {
+        Salesperson salesperson = new Salesperson("Jack", 1500);
+        assertEquals("Jack", salesperson.getName());
+        assertEquals(1500, salesperson.getAmount());
+    }
+
+}
