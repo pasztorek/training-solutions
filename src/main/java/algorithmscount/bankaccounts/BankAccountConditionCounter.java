25dfd2a8fd0d7ac05533b8ff53f24c8b6b8afb9e
pgk <pgk977@outlook.com>
2020-11-16 19:32:05 +0100

    2.

---
+++ src/main/java/algorithmscount/bankaccounts/BankAccountConditionCounter.java
@@ -0,0 +1,12 @@
+package algorithmscount.bankaccounts;
+
+import java.util.List;
+
+public class BankAccountConditionCounter{
+
+    public int countWithBalanceGreaterThan(List<BankAccount> account, int limit){
+
+        return 3;
+    }
+
+}
