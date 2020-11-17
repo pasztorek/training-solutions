559c99cd2d4c0c79db782d589e8d017d33556045
pgk <pgk977@outlook.com>
2020-11-17 09:49:30 +0100

    2.

---
+++ src/main/java/algorithmsdecision/BankAccount.java
@@ -0,0 +1,41 @@
+package algorithmsdecision;
+
+public class BankAccount {
+
+    private String nameOfOwner;
+    private String accountNumber;
+    private int balance;
+
+    public BankAccount(String nameOfOwner, String accountNumber, int balance) {
+        this.nameOfOwner = nameOfOwner;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getNameOfOwner() {
+        return nameOfOwner;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public boolean deposit(int amount) {
+        balance = balance+amount;
+        return true;
+    }
+
+    public boolean withdraw(int amount) {
+        if(balance - amount > 0) {
+            balance = balance - amount;
+            return true;
+        }
+        return false;
+    }
+
+
+}
