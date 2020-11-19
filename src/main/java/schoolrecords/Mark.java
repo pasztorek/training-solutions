9dbb0cb5aa315a60200e0f77d851e2e2d5e7adc6
pgk <pgk977@outlook.com>
2020-11-19 08:38:27 +0100

    2.

--- src/main/java/schoolrecords/Mark.java
+++ src/main/java/schoolrecords/Mark.java
@@ -6,4 +6,28 @@ public class Mark {
     private Subject subject;
     private Tutor tutor;
 
+    public MarkType getMarkType() {
+        return marktype;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+
+
+    public Mark(Subject subject, Tutor tutor) {
+        this.subject = subject;
+        this.tutor = tutor;
+    }
+
+    public Mark(MarkType marktype, Subject subject, Tutor tutor) {
+        this.marktype = marktype;
+        this.subject = subject;
+        this.tutor = tutor;
+    }
 }
--- src/main/java/schoolrecords/Tutor.java
+++ src/main/java/schoolrecords/Tutor.java
@@ -6,5 +6,19 @@ import java.util.List;
 public class Tutor {
 
     private String name;
-    private List<Subject> = new ArrayList()<>
+    private List<Subject> taughtSubject = new ArrayList<>();
+
+    public Tutor(String name, List<Subject> taughtSubject) {
+        this.name = name;
+        this.taughtSubject = taughtSubject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean tutorTeachingSubject(Subject subject) {
+
+        return true;
+    }
 }
