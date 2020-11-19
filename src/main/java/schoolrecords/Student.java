4c7a0c5ebf0250aca5cb94b58d79ffc9e40e13c7
pgk <pgk977@outlook.com>
2020-11-19 22:46:52 +0100

    2.

--- src/main/java/schoolrecords/Student.java
+++ src/main/java/schoolrecords/Student.java
@@ -1,5 +1,6 @@
 package schoolrecords;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,14 +30,24 @@ public class Student {
 
     public double calculateAverage() {
 
-        return 3.0;
+        double avg = 0.00;
+        for (int i = 0; i < marks.size(); i++) {
+
+            avg = marks.get(i).getMarkType().getValue()+avg;
+        }
+
+        avg = avg / marks.size();
+
+        int temp = (int)(avg * 100);
+        avg = ((double)temp)/100;
+
+        return avg;
     }
 
     public double calculateSubjectAverage(Subject subject) {
 
         return 3.0;
     }
-
 
     @Override
     public String toString() {
