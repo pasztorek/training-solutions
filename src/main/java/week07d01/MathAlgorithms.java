a84561f8358f5849f26dfb691ff66421e146a9e5
pgk <pgk977@outlook.com>
2020-12-07 16:04:35 +0100

    2.

--- src/main/java/week07d01/MathAlgorithms.java
+++ src/main/java/week07d01/MathAlgorithms.java
@@ -3,12 +3,16 @@ package week07d01;
 public class MathAlgorithms {
 
 
-    public static boolean isPrime(int x){
-        for(int i=2; i<x ;i++){
-            if(x % i == 0){
-                return false;
-            }
+    public static boolean isPrime(int x) {
+        if (x <= 1) {
+            return false;
         }
-        return true;
+            for (int i = 2; i < x; i++) {
+                if (x % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
-}
+
--- src/test/java/week07d01/MathAlgorithmsTest.java
+++ src/test/java/week07d01/MathAlgorithmsTest.java
@@ -1,19 +1,26 @@
 package week07d01;
 
 import org.junit.Test;
+import week06d04.Budget;
 import week06d05.Biscuit;
 import week06d05.BiscuitType;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MathAlgorithmsTest {
 
     @Test
-    public void test1(){
+    public void test1() {
         assertEquals(true, MathAlgorithms.isPrime(2));
+        assertEquals(false, MathAlgorithms.isPrime(1));
         assertEquals(true, MathAlgorithms.isPrime(18481));
         assertEquals(false, MathAlgorithms.isPrime(10));
         assertEquals(false, MathAlgorithms.isPrime(18482));
 
     }
 }
+
+
+
+
