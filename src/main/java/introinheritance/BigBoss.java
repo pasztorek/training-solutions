b47c093f44facee1592c616939e5870bcdbeab64
pgk <pgk977@outlook.com>
2020-12-13 01:06:08 +0100

    2.

--- src/main/java/introinheritance/BigBoss.java
+++ src/main/java/introinheritance/BigBoss.java
@@ -1,4 +1,27 @@
 package introinheritance;
 
 public class BigBoss {
+
+    double bonus;
+
+
+    public BigBoss(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public int getNumberOfEmployees(){
+
+        return 1;
+    }
+
+    public double getBonus(){
+
+        return 1.0;
+    }
+
+    public double getSalary(){
+
+        return 1.0;
+    }
+
 }
--- src/main/java/introinheritance/Boss.java
+++ src/main/java/introinheritance/Boss.java
@@ -2,23 +2,31 @@ package introinheritance;
 
 public class Boss extends Employee {
 
-    double LEADERSHIP_FACTOR = 0.1;
+    double LEADERSHIP_FACTOR=0.1;
+    int NumberOfEmployees;
 
 
     public Boss(String name, String address, double salary, double LEADERSHIP_FACTOR) {
         super(name, address, salary);
-        this.LEADERSHIP_FACTOR = LEADERSHIP_FACTOR;
+        this.LEADERSHIP_FACTOR = this.LEADERSHIP_FACTOR*LEADERSHIP_FACTOR;
     }
 
     public double getSalary() {
 
-        return salary = salary + (salary*LEADERSHIP_FACTOR);
+        return salary + (salary*LEADERSHIP_FACTOR);
     }
 
-    public int getNumberOfEmployees() {
-
-
-        return 1;
+    public double getNumberOfEmployees() {
+        return LEADERSHIP_FACTOR;
     }
 
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String getAddress() {
+        return super.getAddress();
+    }
 }
