e35808894c73023b64600883f6df881b266c669a
pgk <pgk977@outlook.com>
2021-01-13 20:09:31 +0100

    2.

--- src/main/java/week11d02/Courier.java
+++ src/main/java/week11d02/Courier.java
@@ -8,17 +8,29 @@ public class Courier {
     private List<Ride> rides = new ArrayList<>();
 
     public void addRide(Ride ride) {
+
+        if(rides.size()>0){
+
+            if(ride.getDay() < rides.get(rides.size()-1).getDay()){
+                throw new IllegalArgumentException("Nem jó paraméter");
+            }
+
+            if((ride.getDay() == rides.get(rides.size()-1).getDay()) && (ride.getCounter() < rides.get(rides.size()-1).getCounter())){
+                throw new IllegalArgumentException("Nem jó paraméter");
+            }
+        }
+
         rides.add(ride);
     }
 
     public int getFirstHoliday() {
-    //    int lastDay=rides.get(0).getDay();
 
         for(int i=0; i<rides.size(); i++){
             if((rides.get(i+1).getDay()-rides.get(i).getDay()) > 1){
-                return i+1;
+                return rides.get(i).getDay()+1;
             }
         }
-        return 99;
+
+        return 0;
     }
 }
--- src/test/java/week11d02/CourierTest.java
+++ src/test/java/week11d02/CourierTest.java
@@ -1,7 +1,7 @@
 package week11d02;
 
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CourierTest {
 
@@ -15,11 +15,51 @@ public class CourierTest {
         Ride rd4 = new Ride(4, 1, 19);
 
         Courier cr = new Courier();
+
         cr.addRide(rd1);
         cr.addRide(rd2);
         cr.addRide(rd3);
         cr.addRide(rd4);
 
+        assertEquals(3,cr.getFirstHoliday());
+
+    }
+
+    @Test
+    public void test2(){
+
+        Ride rd1 = new Ride(1, 1, 12);
+        Ride rd2 = new Ride(2, 1, 11);
+        Ride rd3 = new Ride(1, 1, 10);
+
+
+        Courier cr = new Courier();
+
+        cr.addRide(rd1);
+        cr.addRide(rd2);
+
+
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> cr.addRide(rd3));
+        assertEquals("Nem jó paraméter", ex.getMessage());
+
+    }
+
+    @Test
+    public void test3(){
+
+        Ride rd1 = new Ride(1, 1, 12);
+        Ride rd2 = new Ride(1, 2, 11);
+        Ride rd3 = new Ride(1, 1, 10);
+
+
+        Courier cr = new Courier();
+
+        cr.addRide(rd1);
+        cr.addRide(rd2);
+
+
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> cr.addRide(rd3));
+        assertEquals("Nem jó paraméter", ex.getMessage());
 
     }
 
