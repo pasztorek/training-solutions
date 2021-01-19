f26b6f7bf925ff0e25c1d73d463e524305f2329d
pgk <pgk977@outlook.com>
2021-01-19 22:52:25 +0100

    2.

--- src/main/java/week12d02/Site.java
+++ src/main/java/week12d02/Site.java
@@ -4,7 +4,23 @@ public class Site {
 
     boolean side;
     int lenght;
-    int number;
     Fence fence;
 
+    public Site(boolean side, int lenght, Fence fence) {
+        this.side = side;
+        this.lenght = lenght;
+        this.fence = fence;
+    }
+
+    public boolean isSide() {
+        return side;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public Fence getFence() {
+        return fence;
+    }
 }
--- src/main/java/week12d02/Street.java
+++ src/main/java/week12d02/Street.java
@@ -1,6 +1,15 @@
 package week12d02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Street {
 
+    private List<Site> sites= new ArrayList<>();
+
+    public void sellSite(Site site){
+
+
+   }
 
 }
