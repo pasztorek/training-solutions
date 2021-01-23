e15820e010fcc0fb1d1203f7786c399c684588e9
pgk <pgk977@outlook.com>
2021-01-23 09:14:42 +0100

    2.

---
+++ src/main/java/week12d05/IndexCounter.java
@@ -0,0 +1,37 @@
+package week12d05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class IndexCounter {
+
+
+    public int getNumber(){
+        Path index = Path.of("index.html");
+        int counter=0;
+        try (BufferedReader reader = Files.newBufferedReader(index)) {
+            String line;
+
+
+
+            while((line = reader.readLine()) != null) {
+                //System.out.println(line);
+                if(line.toLowerCase().contains("koronavírus") || line.toLowerCase().contains("koronavirus")){
+                    counter++;
+                }
+            }
+        }
+        catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        IndexCounter ic = new IndexCounter();
+        System.out.println(ic.getNumber());
+    }
+
+}
