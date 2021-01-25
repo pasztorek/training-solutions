b7d05e86a76ba629a0c418727a017e42435366c6
pgk <pgk977@outlook.com>
2021-01-25 21:37:33 +0100

    2.

--- src/main/java/week13d01/getTown.java
+++ src/main/java/week13d01/getTown.java
@@ -7,17 +7,32 @@ import java.io.InputStreamReader;
 public class getTown {
 
     public String getLongestTownName(){
+            String longestTown="";
+            String line;
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(getTown.class.getResourceAsStream("/iranyitoszamok-varosok-2021.csv")))){
 
 
 
+            while((line = reader.readLine()) != null) {
+
+                String[] town = line.split(";");
+
+                if(longestTown.length() < town[1].length()){
+                    longestTown = town[1];
+
+                }
+                // System.out.println(line);
+            }
         }
             catch (IOException ioe){
             throw new IllegalStateException("Cannot read file", ioe);
-
             }
-        return null;
+        return longestTown;
     }
 
+    public static void main(String[] args) {
+        getTown gt = new getTown();
+        System.out.println(gt.getLongestTownName());
+    }
 }
