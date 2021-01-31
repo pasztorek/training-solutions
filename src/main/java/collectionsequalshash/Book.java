cbf4c7610c620a59efaab643d26d74f5e60519fb
pgk <pgk977@outlook.com>
2021-01-31 18:18:10 +0100

    2.

--- src/main/java/collectionsequalshash/Book.java
+++ src/main/java/collectionsequalshash/Book.java
@@ -1,5 +1,7 @@
 package collectionsequalshash;
 
+import java.util.Objects;
+
 public class Book {
 
     private String RegNumber;
@@ -27,5 +29,28 @@ public class Book {
 
     public String getAuthor() {
         return Author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return title.equals(book.title) &&
+                Author.equals(book.Author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, Author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "regNumber='" + RegNumber + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + Author + '\'' +
+                '}';
     }
 }
--- src/main/java/collectionsequalshash/BookCatalog.java
+++ src/main/java/collectionsequalshash/BookCatalog.java
@@ -1,6 +1,7 @@
 package collectionsequalshash;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -10,19 +11,34 @@ public class BookCatalog {
 
 
     public Book findBookByTitleAuthor(List<Book> books, String title, String author){
+        Book result=null;
 
-        return null;
+        for(Book book: books){
+            if(book.equals(new Book(title,author))){
+                result = book;
+            }
+        }
+        return result;
     }
 
     public Book findBook(List<Book> books, Book book){
+        Book result=null;
 
+        for(Book findBook: books){
+            if(book.equals(findBook)){
+                result = book;
+            }
+        }
+        return result;
 
-        return null;
     }
 
     public Set<Book> addBooksToSet(Book[] books){
+        Set <Book> samebooks = new HashSet<>();
+        for(Book bk :books){
+            samebooks.add(bk);
+        }
 
-
-        return null;
+        return samebooks;
     }
 }
