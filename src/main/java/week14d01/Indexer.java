0022fa97943f5d3bba5f0ed1296a1d3feabd88fe
pgk <pgk977@outlook.com>
2021-02-01 19:15:35 +0100

    2.

--- src/main/java/week14d01/Indexer.java
+++ src/main/java/week14d01/Indexer.java
@@ -1,5 +1,6 @@
 package week14d01;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,15 +11,28 @@ public class Indexer {
     public Map<Character, List<String>> index (List<String> names){
         Map<Character, List<String>> result = new HashMap<>();
 
+        for(String str: names){
+            result.put(str.charAt(0), new ArrayList<>());
+        }
 
+        for(Character chr: result.keySet()){
+            List<String> namesPerChar = new ArrayList<>();
 
-        return null;
+            for(String str: names){
+                if(str.charAt(0) == chr){
+                    namesPerChar.add(str);
+                }
+                result.put(chr, namesPerChar);
+            }
+
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
         Indexer idx = new Indexer();
         idx.index(List.of("Aranka", "Amália", "Balázs", "Boldizsár", "Celília", "Dezső", "Dénes", "Elemér"));
-
     }
 
 }
