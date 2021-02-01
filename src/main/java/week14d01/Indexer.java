8c645e272b3f3627ff64e85607206bc99c706472
pgk <pgk977@outlook.com>
2021-02-01 18:16:58 +0100

    2.

---
+++ src/main/java/week14d01/Indexer.java
@@ -0,0 +1,24 @@
+package week14d01;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Indexer {
+
+
+    public Map<Character, List<String>> index (List<String> names){
+        Map<Character, List<String>> result = new HashMap<>();
+
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Indexer idx = new Indexer();
+        idx.index(List.of("Aranka", "Amália", "Balázs", "Boldizsár", "Celília", "Dezső", "Dénes", "Elemér"));
+
+    }
+
+}
