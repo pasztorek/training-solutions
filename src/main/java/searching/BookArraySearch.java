1093c99fda20997fa8b96d4ac78d29c4ad982f29
pgk <pgk977@outlook.com>
2021-02-07 19:20:03 +0100

    2.

---
+++ src/main/java/searching/BookArraySearch.java
@@ -0,0 +1,16 @@
+package searching;
+
+public class BookArraySearch {
+
+    private Book[] bookArray;
+
+    public BookArraySearch(Book[] bookArray) {
+        this.bookArray = bookArray;
+    }
+
+    public Book findBookByAuthorTitle(String title, String author){
+
+
+        return null;
+    }
+}
