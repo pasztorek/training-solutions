e25d66bf913bf1517d4a40849982dfd1215c7e5d
pgk <pgk977@outlook.com>
2021-02-07 15:46:19 +0100

    2.

--- src/main/java/collectionscomp/Book.java
+++ src/main/java/collectionscomp/Book.java
@@ -2,5 +2,25 @@ package collectionscomp;
 
 public class Book {
 
+    private int RegNumber;
+    private String title;
+    private String Author;
 
+    public Book(int regNumber, String title, String author) {
+        RegNumber = regNumber;
+        this.title = title;
+        Author = author;
+    }
+
+    public int getRegNumber() {
+        return RegNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
 }
--- src/main/java/collectionscomp/OrderedLibrary.java
+++ src/main/java/collectionscomp/OrderedLibrary.java
@@ -1,4 +1,30 @@
 package collectionscomp;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class OrderedLibrary {
+
+    private List<Book> books = new ArrayList<>();
+
+    public OrderedLibrary(List<Book> books) {
+        this.books = books;
+    }
+
+    public List<Book> orderedByTitle(){
+
+        return null;
+    }
+
+    public List<Book> orderedByAuthor(){
+
+        return null;
+    }
+
+    public List<String> orderedByTitleLocale(Locale locl){
+
+        return null;
+    }
+
 }
