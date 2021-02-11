4689c61804f2ca82f53bc15ba78ec73e79144da4
pgk <pgk977@outlook.com>
2021-02-11 20:42:28 +0100

    2.

--- src/main/java/week15d04/Covid19.java
+++ src/main/java/week15d04/Covid19.java
@@ -3,11 +3,11 @@ package week15d04;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Covid19 {
-    Map<String, Double> result = new HashMap<>();
+    Map<String, Double> filtered = new HashMap<>();
+    Map<String, Double> ordered = new LinkedHashMap<>();
 
     public void readFile(String file){
 
@@ -21,8 +21,12 @@ public class Covid19 {
 
                 if (line.contains("Hungary")) {
                     week = line.substring(line.indexOf('-') + 1, line.indexOf('-') + 3);
-                    cases = Double.parseDouble(line.substring(line.indexOf("e,")+2, line.length()));
-                    result.put(week,cases);
+
+                    if(!line.substring(line.indexOf("e,")+2, line.length()).isEmpty()){
+                        cases = Double.parseDouble(line.substring(line.indexOf("e,")+2, line.length()));
+                        filtered.put(week,cases);
+                    }
+
                 }
             }
         }
@@ -32,9 +36,33 @@ public class Covid19 {
         }
     }
 
+    public Map<String, Double> ordering(){
+        Map.Entry<String, Double> maximum = null;
+        Double max = 0.0;
+
+
+        for(int i=0; i<3; i++){
+        for(Map.Entry<String, Double> entry: filtered.entrySet()) {
+
+            if (entry.getValue() > max) {
+                max= entry.getValue();
+                maximum = entry;
+
+            }
+
+            }
+            ordered.put(maximum.getKey(), maximum.getValue());
+            filtered.remove(maximum.getKey());
+            max =0.0;
+        }
+
+        return ordered;
+    }
+
     public static void main(String[] args) {
         Covid19 c19 = new Covid19();
         c19.readFile("/data.csv");
+        System.out.println(c19.ordering().toString());
 
     }
 }
