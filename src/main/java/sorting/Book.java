160b3f26ee45ce628a41ca07e9bc70179d259256
pgk <pgk977@outlook.com>
2021-02-14 17:12:38 +0100

    2.

---
+++ src/main/java/sorting/Book.java
@@ -0,0 +1,41 @@
+package sorting;
+
+public class Book implements Comparable<Book> {
+
+    private int id;
+    private String title;
+    private String author;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
---
+++ src/main/java/sorting/OrderedArrayLibrary.java
@@ -0,0 +1,21 @@
+package sorting;
+
+public class OrderedArrayLibrary {
+
+    private Book[] bookArray;
+
+    public OrderedArrayLibrary(Book[] bookArray) {
+        this.bookArray = bookArray;
+    }
+
+    public Book[] sortingById(){
+
+        return null;
+    }
+
+
+    public Book[] sortingByTitle(){
+
+        return null;
+    }
+}
