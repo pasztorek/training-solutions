f6836a789b17511d777e7248762fb694ea795313
pgk <pgk977@outlook.com>
2021-02-15 19:15:41 +0100

    2.

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,4 +1,37 @@
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
 public class Histogram {
+
+    public Histogram() {
+    }
+
+    public String createHistogram(BufferedReader br) {
+        String result ="";
+
+       try  {
+            String line;
+            int number=0;
+
+
+            while ((line = br.readLine()) != null) {
+               number =Integer.parseInt(line);
+               for (int i=0; i<number; i++){
+                   result =  result + "*";
+               }
+
+                result =  result + "\n";
+            }
+
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+
+        return result;
+    }
+
 }
