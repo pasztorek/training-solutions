ac8b1c0ac9d73eacd11ade149a1d172d8cdbf85b
pgk <pgk977@outlook.com>
2021-02-15 20:13:15 +0100

    2.

--- src/main/java/exam03/Cruise.java
+++ src/main/java/exam03/Cruise.java
@@ -1,9 +1,7 @@
 package exam03;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Cruise {
 
@@ -13,34 +11,33 @@ public class Cruise {
     private List<Passenger> passengers = new ArrayList<>();
 
 
+    public void bookPassenger(Passenger passenger) {
 
-    public void bookPassenger(Passenger passenger){
-
-        if(boat.getMaxPassengers()<=passengers.size()){
+        if (boat.getMaxPassengers() <= passengers.size()) {
             throw new IllegalArgumentException("Nincs már hely");
         }
         this.passengers.add(passenger);
 
     }
 
-    public double getPriceForPassenger(Passenger passenger){
+    public double getPriceForPassenger(Passenger passenger) {
 
-        return passenger.getCruiseClass().getPriceRate()*basicPrice;
+        return passenger.getCruiseClass().getPriceRate() * basicPrice;
     }
 
-    public Passenger findPassengerByName(String name){
-        for(Passenger psg: passengers){
-            if(psg.getName().equals(name)){
+    public Passenger findPassengerByName(String name) {
+        for (Passenger psg : passengers) {
+            if (psg.getName().equals(name)) {
                 return psg;
             }
         }
         return null;
     }
 
-    public List<String> getPassengerNamesOrdered(){
+    public List<String> getPassengerNamesOrdered() {
         List<String> result = new ArrayList<>();
 
-        for(Passenger psg: passengers){
+        for (Passenger psg : passengers) {
             result.add(psg.getName());
         }
         Collections.sort(result);
@@ -48,11 +45,35 @@ public class Cruise {
         return result;
     }
 
+    public double sumAllBookingsCharged() {
+        double sum=0.0;
+
+        for(Passenger psg: passengers){
+            sum = sum+getPriceForPassenger(psg);
+        }
+
+        return sum;
+    }
 
     public Cruise(Boat boat, LocalDate sailing, double basicPrice) {
         this.boat = boat;
         this.sailing = sailing;
         this.basicPrice = basicPrice;
+    }
+
+    public Map<CruiseClass, Integer> countPassengerByClass(){
+        Map<CruiseClass, Integer> result = new HashMap<>();
+
+        for(Passenger psg: passengers){
+            if(result.containsKey(psg.getCruiseClass())){
+                result.put(psg.getCruiseClass(), result.get(psg.getCruiseClass())+1);
+            }
+            else{
+                result.put(psg.getCruiseClass(),1);
+            }
+        }
+
+        return result;
     }
 
     public Boat getBoat() {
