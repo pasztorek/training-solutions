f7e496d7060a7804e9fb91ecfcda54f3029cd233
pgk <pgk977@outlook.com>
2021-02-25 20:08:11 +0100

    2.

--- src/main/java/activitytracker/ActivityTrackerMain.java
+++ src/main/java/activitytracker/ActivityTrackerMain.java
@@ -6,6 +6,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 public class ActivityTrackerMain {
@@ -41,19 +42,22 @@ public class ActivityTrackerMain {
 
     public void insertData(DataSource ds){
 
-        Activity act1 = new Activity(LocalDateTime.of(2021,02, 01,10,10, 10), "Hegymászás", ActivityType.HIKING);
-        Activity act2 = new Activity(LocalDateTime.of(2021,02, 01,10,10, 10), "Futás", ActivityType.RUNNING);
-        Activity act3 = new Activity(LocalDateTime.of(2021,02, 01,10,10, 10), "Bicajozás", ActivityType.BIKING);
+        Activity act1 = new Activity(LocalDateTime.of(2021,02, 01,10,10), "Hegymászás", ActivityType.HIKING);
+       // Activity act2 = new Activity(LocalDateTime.of(2021,02, 01,10,10), "Futás", ActivityType.RUNNING);
+       // Activity act3 = new Activity(LocalDateTime.of(2021,02, 01,10,10), "Bicajozás", ActivityType.BIKING);
 
         try(
         Connection conn = ds.getConnection()){
-            PreparedStatement stmt = conn.prepareStatement("INSERT INTO activities (start_time, activity_type, activity_desc), values(?,?,?)");
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO activities (start_time, activity_type, activity_desc) values(?,?,?)");
+            stmt.setTimestamp(1, Timestamp.valueOf(act1.getStartTime()));
+            stmt.setString(2, act1.getType().toString());
+            stmt.setString(3, act1.getDesc());
+            stmt.executeUpdate();
 
 
         } catch (SQLException se) {
-            throw new IllegalArgumentException(" Nem jó", se);
+            throw new IllegalArgumentException(" Nem jó...", se);
         }
-
 
     }
 
