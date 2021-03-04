81f82ba7395aff4734225d16afc97fe5606b31f5
pgk <pgk977@outlook.com>
2021-03-04 08:09:37 +0100

    2.

--- src/main/java/covid/Citizen.java
+++ src/main/java/covid/Citizen.java
@@ -1,5 +1,68 @@
 package covid;
 
+import java.time.LocalDate;
+
 public class Citizen {
 
+private String name;
+private String zip;
+private int age;
+private String email;
+private int numOfVaccine;
+private LocalDate lastVaccination;
+
+    public Citizen(String name, String zip, int age, String email) {
+        this.name = name;
+        this.zip = zip;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getNumOfVaccine() {
+        return numOfVaccine;
+    }
+
+    public void setNumOfVaccine(int numOfVaccine) {
+        this.numOfVaccine = numOfVaccine;
+    }
+
+    public LocalDate getLastVaccination() {
+        return lastVaccination;
+    }
+
+    public void setLastVaccination(LocalDate lastVaccination) {
+        this.lastVaccination = lastVaccination;
+    }
 }
--- src/main/java/covid/CovidMain.java
+++ src/main/java/covid/CovidMain.java
@@ -1,6 +1,66 @@
 package covid;
 
+import java.util.Scanner;
+
 public class CovidMain {
 
+    public static void main(String[] args) {
+
+        CovidMain cvdm = new CovidMain();
+
+        System.out.println("1. Regisztráció");
+        System.out.println("2. Tömeges regisztráció");
+        System.out.println("3. Generálás");
+        System.out.println("4. Oltás");
+        System.out.println("5. Oltás meghiúsulás");
+
+        Scanner scanner = new Scanner(System.in);
+        int menuId = scanner.nextInt();
+
+        if(menuId==1){
+            cvdm.manualRegistration(1);
+        }
+
+
+    }
+
+
+    public void manualRegistration(int munuId){
+        System.out.println("REGISZTRÁCIÓ");
+        System.out.println("Név:");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Irányítószám:");
+        String zip = scanner.nextLine();
+
+        System.out.println("Életkor:");
+
+        System.out.println("E-mail cím:");
+
+        System.out.println("TAJ szám:");
+
+
+
+
+
+    }
+
+    public void massRegistration(int munuId){
+
+    }
+
+    public void generating(int munuId){
+
+    }
+
+    public void vaccinacion(int munuId){
+
+    }
+
+    public void vaccinationFail(int munuId){
+
+    }
 
 }
